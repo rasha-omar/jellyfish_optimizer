911987659cdd1641f38bde3ad82bb34c6c42178f
rasha-omar <105171059+rasha-omar@users.noreply.github.com>
2023-12-14 14:37:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jellyfish_optimizer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        jellyfish_optimizer/
+    setup.py
+    jellyfish_optimizer/
+        __init__.py
+        module1.py
+        HeartDisease.py
+        # ... other Python files
+README.md
+LICENSE
+# ... other files
+
+    ],
+)
